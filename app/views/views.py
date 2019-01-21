5b0dbc6225121733474d3e1f6d5bf68119fed5e9
nanfuka <kalungi2k6@gmail.com>
2019-01-21 20:49:25 +0300

    Modify views

--- app/views/views.py
+++ app/views/views.py
@@ -1,11 +1,11 @@
 from flask import Flask, jsonify, request, json
 from app.controllers.user_controllers import User_controller
-# from app.models.incident import Incident, incidents
+from app.validators import Validators
 
 
 app = Flask(__name__)
-# users = User()
-
+validators = Validators()
+user_controller = User_controller()
 
 @app.route('/')
 def index():
@@ -27,43 +27,41 @@ def signup():
     password = data.get('password')
     user = User_controller()
 
-    # invalid_user_input = validators.validate_strings(firstname, lastname, othernames, username, data)
-    # if invalid_user_input:
-    #     return jsonify({"status": 400, 'error': invalid_user_input}), 400 
-    # invalid_email = validators.validate_email(email)
-    # if invalid_email:
-    #     return jsonify({"status": 400, 'error': invalid_email}), 400 
-    # invalid_type = validators.validat_numbers(phoneNumber)
-    # if invalid_type:
-    #     return jsonify({"status": 400, 'error': invalid_type}), 400 
-    # validate_boolean = validators.validate_boolean(isAdmin)
-    # if validate_boolean:
-    #     return jsonify({"status": 400, 'error': validate_boolean}), 400
-    # validate_password = validators.validate_password(password)
-    # if validate_password:
-    #     return jsonify({"status": 400, 'error': validate_password}), 400
+    invalid_user_input = validators.validate_strings(firstname, lastname, othernames, username, data)
+    if invalid_user_input:
+        return jsonify({"status": 400, 'error': invalid_user_input}), 400 
+    invalid_email = validators.validate_email(email)
+    if invalid_email:
+        return jsonify({"status": 400, 'error': invalid_email}), 400 
+    invalid_type = validators.validat_numbers(phoneNumber)
+    if invalid_type:
+        return jsonify({"status": 400, 'error': invalid_type}), 400 
+    validate_boolean = validators.validate_boolean(isAdmin)
+    if validate_boolean:
+        return jsonify({"status": 400, 'error': validate_boolean}), 400
+    validate_password = validators.validate_password(password)
+    if validate_password:
+        return jsonify({"status": 400, 'error': validate_password}), 400
         
-    # invalid_detail = user.check_repitition(username, email, password)
-    # if invalid_detail:
-    #     return jsonify({"status": 400, 'error': invalid_detail}), 400
+    invalid_detail = user_controller.check_repitition(username, email, password)
+    if invalid_detail:
+        return jsonify({"status": 400, 'error': invalid_detail}), 400
 
-    # elif invalid_detail:
-    #     return jsonify({"status": 400, 'error': invalid_detail}), 400
-    # else:
-    newuserinput = user.signup(data['firstname'], data['lastname'], data['othernames'], data['email'], data['phoneNumber'], data['username'], data['isAdmin'], data['password'])
-        #     data['firstname'],
-        #     data['lastname'],
-        #     data['othernames'],
-        #     data['email'],
-        #     data['phoneNumber'],
-        #     data['username'],
-        #     data['isAdmin'],
-        #     data['password'])
+    else:
+        newuserinput = user.signup(
+            data['firstname'],
+            data['lastname'],
+            data['othernames'],
+            data['email'],
+            data['phoneNumber'],
+            data['username'],
+            data['isAdmin'],
+            data['password'])
    
-    return jsonify({
-        "status": 201,
-        "message": "Successfully signedup with ireporter",
-        "data": newuserinput}), 201
+        return jsonify({
+                "status": 201,
+                "message": "Successfully signedup with ireporter",
+                "data": newuserinput}), 201
 
 
 
